588669d9a9b233376c2dd8970b9a0ceeb8f9496e
Leandro <leaxavier2014@gmail.com>
2021-10-23 19:47:14 -0500

    añadiendo carpeta de router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Router>
+                <Switch>
+                    
+                </Switch>
+            </Router>
+        </>
+    )
+}
